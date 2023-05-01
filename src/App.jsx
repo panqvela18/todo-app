9134e80a71b8e51e5269f865fa98ce4ba8e49c2e
irakli pankvelashvili <irakli.pankvelashvili@gmail.com>
2023-05-01 22:55:38 +0400

    fixed bg image problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
+import img from "./assets/bg-mobile-light.jpg"
 function App() {
   const [theme,setTheme] = useState("light")
   return (
     <>
       <div className={`w-screen h-screen ${theme === "light" ? "bg-white" : "bg-bgDark"} overflow-x-hidden `}>
-      <div className={`${theme === "light" ? "bg-lightBgImage lg:bg-light-bg-image-desk" : "bg-dark-bg-image lg:bg-dark-bg-image-desk"} w-screen h-1/3 bg-no-repeat bg-cover px-6  lg:px-450`}>
+      <div className={`${theme === "light" ? "bg-[url('./assets/bg-mobile-light.jpg')] lg:bg-light-bg-image-desk" : "bg-dark-bg-image lg:bg-dark-bg-image-desk"} w-screen h-1/3 bg-no-repeat bg-cover px-6  lg:px-450`}>
         <Header theme={theme} setTheme={setTheme} />
         <TodoList theme={theme} />
       </div>
